4985114a6250693ecdeae9dcab12df6ac1c40529
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-08-15 16:31:01 -0400

    backup

--- go.mod
+++ go.mod
@@ -1,34 +1,36 @@
 module orca-rotor
 
-go 1.22.5
+go 1.24.0
+
+toolchain go1.24.5
 
 require (
-	gioui.org v0.8.0
 	github.com/c-loftus/orca-controller v0.0.0-20250727151856-b445746eb976
 	github.com/charmbracelet/log v0.4.2
+	github.com/kbinani/screenshot v0.0.0-20250624051815-089614a94018
 	golang.design/x/hotkey v0.4.1
 )
 
 require (
-	gioui.org/shader v1.0.8 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
 	github.com/charmbracelet/lipgloss v1.1.0 // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/gen2brain/shm v0.1.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/go-text/typesetting v0.2.1 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/ollama/ollama v0.11.4 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/exp v0.0.0-20240707233637-46b078467d37 // indirect
-	golang.org/x/exp/shiny v0.0.0-20240707233637-46b078467d37 // indirect
-	golang.org/x/image v0.18.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
+	golang.org/x/sys v0.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"time"
+
+	"fmt"
+	"image/png"
+	"os"
 
 	oc "github.com/c-loftus/orca-controller"
 	"github.com/charmbracelet/log"
+	"github.com/kbinani/screenshot"
+	"github.com/ollama/ollama/api"
 	"golang.design/x/hotkey"
 )
+
+func takeScreenshot() (string, error) {
+	const activeDisplayIndex = 0
+	bounds := screenshot.GetDisplayBounds(activeDisplayIndex)
+
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		return "", err
+	}
+	fileName := fmt.Sprintf("%d_%dx%d.png", activeDisplayIndex, bounds.Dx(), bounds.Dy())
+	file, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	if err := png.Encode(file, img); err != nil {
+		return "", err
+	}
+
+	return fileName, nil
+}
 
 func createClient() *oc.OrcaClient {
 	for {
@@ -48,8 +75,86 @@ func handleKeys() error {
 		return err
 	}
 
+	toggleSpeech := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyF8)
+	err = toggleSpeech.Register()
+	if err != nil {
+		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+		return err
+	}
+
+	processScreenshot := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyF9)
+	err = processScreenshot.Register()
+	if err != nil {
+		log.Fatalf("hotkey: failed to register hotkey: %v", err)
+		return err
+	}
+
 	for {
 		select {
+
+		case <-toggleSpeech.Keydown():
+			err := client.SpeechAndVerbosityManager.ToggleSpeech(true)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+		case <-processScreenshot.Keydown():
+			name, err := takeScreenshot()
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			err = client.PresentMessage("Screenshot taken")
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			ollamaClient, err := api.ClientFromEnvironment()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			asBytes, err := os.ReadFile(name)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			messages := api.Message{
+				Role:   "user",
+				Images: []api.ImageData{asBytes},
+			}
+
+			chatReq := api.ChatRequest{
+				Model: "qwen2.5vl",
+				Messages: []api.Message{
+					messages,
+				},
+			}
+			var allContent string
+			respFunc := func(resp api.ChatResponse) error {
+				allContent += resp.Message.Content
+				return nil
+			}
+			client.PresentMessage("Processing screenshot...")
+			log.Info("Processing screenshot...")
+			if err := ollamaClient.Chat(context.Background(), &chatReq, respFunc); err != nil {
+				log.Error(err)
+				continue
+			}
+			log.Info(allContent)
+
+			err = client.PresentMessage(allContent)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			err = os.Remove(name)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
 		case <-raiseSpeed.Keydown():
 
 			err := client.SpeechAndVerbosityManager.InterruptSpeech(true)
@@ -103,7 +208,43 @@ func handleKeys() error {
 }
 
 func main() {
-	err := handleKeys()
+
+	ollamaClient, err := api.ClientFromEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	models, err := ollamaClient.List(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	var foundQwen bool
+	for _, model := range models.Models {
+		if model.Name == "qwen2.5vl:latest" {
+			foundQwen = true
+			break
+		}
+	}
+
+	if !foundQwen {
+		log.Info("qwen2.5vl not found; pulling")
+		req := &api.PullRequest{
+			Model: "qwen2.5vl",
+		}
+		progressFunc := func(resp api.ProgressResponse) error {
+			log.Info("Progress: status=%v, total=%v, completed=%v\n", resp.Status, resp.Total, resp.Completed)
+			return nil
+		}
+
+		err = ollamaClient.Pull(context.Background(), req, progressFunc)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Info("qwen2.5vl vision model found; skipping pull")
+	}
+
+	err = handleKeys()
 	if err != nil {
 		log.Fatal(err)
 	}
